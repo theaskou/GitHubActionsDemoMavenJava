c20729504dc49f142238c5ec2e5d6045cbfcb7d3
theaskou <thsk0001@stud.kea.dk>
2024-02-27 14:48:59 +0100

    1st

--- src/main/java/com/example/githubactionsdemomavenjava/GitHubActionsDemoMavenJavaApplication.java
+++ src/main/java/com/example/githubactionsdemomavenjava/GitHubActionsDemoMavenJavaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitHubActionsDemoMavenJavaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GitHubActionsDemoMavenJavaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubActionsDemoMavenJavaApplication.class, args);
+    }
 
 }
