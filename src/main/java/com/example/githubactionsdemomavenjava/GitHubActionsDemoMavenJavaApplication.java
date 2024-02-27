aad6955ac05cbc7b28b233951b2489f884b0aab5
theaskou <thsk0001@stud.kea.dk>
2024-02-27 14:44:23 +0100

    Initial commit

---
+++ src/main/java/com/example/githubactionsdemomavenjava/GitHubActionsDemoMavenJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsdemomavenjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubActionsDemoMavenJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubActionsDemoMavenJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/githubactionsdemomavenjava/GitHubActionsDemoMavenJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsdemomavenjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubActionsDemoMavenJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
