fdad6e880a046caad91436dc1157b82f5ad66191
theaskou <thsk0001@stud.kea.dk>
2024-02-28 09:42:06 +0100

    Request Mapping

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,9 +1,12 @@
 package controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.stereotype.Controller
 public class Controller {
+    
+    @RequestMapping("/welcome")
 
     @GetMapping("")
     public String welcome(){
