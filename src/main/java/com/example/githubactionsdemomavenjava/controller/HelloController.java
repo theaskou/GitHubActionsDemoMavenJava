bbfa10be94d1b58f86322c2a1fcec0b4cc9bd6cf
theaskou <thsk0001@stud.kea.dk>
2024-03-01 11:54:05 +0100

    helloController

--- src/main/java/com/example/githubactionsdemomavenjava/controller/HelloController.java
+++ src/main/java/com/example/githubactionsdemomavenjava/controller/HelloController.java
@@ -1,10 +1,13 @@
-package controller;
+package com.example.githubactionsdemomavenjava.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@org.springframework.stereotype.Controller
-public class Controller {
+@Controller
+@RequestMapping("")
+
+public class HelloController {
     
     @RequestMapping("/welcome")
 
